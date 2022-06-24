212c8f4f8e0fc123948b64aea9d6dceafc5d6e67
Ahira Adefokun <justiceahira@gmail.com>
2022-06-24 01:51:53 +0100

    Remove roles reverse reference on Authority

--- src/main/java/com/ahirajustice/customersupport/common/entities/Authority.java
+++ src/main/java/com/ahirajustice/customersupport/common/entities/Authority.java
@@ -1,6 +1,5 @@
 package com.ahirajustice.customersupport.common.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,14 +7,11 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import java.util.Set;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity(name = "authorities")
 public class Authority extends BaseEntity {
 
@@ -24,9 +20,6 @@ public class Authority extends BaseEntity {
 
     @Column(nullable = false)
     private boolean isSystem;
-
-    @ManyToMany(mappedBy = "authorities")
-    private Set<Role> roles;
 
     public Authority(String name) {
         this.name = name;
