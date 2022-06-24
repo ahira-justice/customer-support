3369ac2baf6444f295064b83539f7e91aa95401c
Ahira Adefokun <justiceahira@gmail.com>
2022-06-24 01:47:44 +0100

    See c3dc5e0c

--- src/main/java/com/ahirajustice/customersupport/common/repositories/ConversationRepository.java
+++ src/main/java/com/ahirajustice/customersupport/common/repositories/ConversationRepository.java
@@ -3,9 +3,13 @@ package com.ahirajustice.customersupport.common.repositories;
 import com.ahirajustice.customersupport.common.entities.Agent;
 import com.ahirajustice.customersupport.common.entities.Conversation;
 import com.ahirajustice.customersupport.common.entities.User;
+import com.ahirajustice.customersupport.common.enums.ConversationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,5 +19,12 @@ public interface ConversationRepository extends JpaRepository<Conversation, Long
     List<Conversation> findAllByAgent(Agent agent);
 
     List<Conversation> findAllByUser(User user);
+
+    @Transactional
+    @Modifying
+    @Query("update #{#entityName} conversation " +
+            "set conversation.agent.id = :agentId, conversation.status = :status " +
+            "where conversation.id = :conversationId")
+    void updateAgentIdAndStatus(long conversationId, long agentId, ConversationStatus status);
 
 }
