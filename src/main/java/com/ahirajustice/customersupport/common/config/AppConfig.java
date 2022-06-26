3976a2009383d9838000c9bcf846babfdc0bad6b
Ahira Adefokun <justiceahira@gmail.com>
2022-06-26 12:02:19 +0100

    Modify objectmapper config

--- src/main/java/com/ahirajustice/customersupport/common/config/AppConfig.java
+++ src/main/java/com/ahirajustice/customersupport/common/config/AppConfig.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -44,13 +43,12 @@ public class AppConfig {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = JsonMapper.builder()
-				.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-				.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-				.build();
+		ObjectMapper mapper = new ObjectMapper();
 
 		mapper.registerModule(new JavaTimeModule());
+		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
 
 		return mapper;
 	}
