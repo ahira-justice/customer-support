58848a2cc987bc54e631b7eb9bd0b7014b6118b0
Ahira Adefokun <justiceahira@gmail.com>
2023-10-15 09:59:25 +0100

    Refactor

--- src/main/java/com/ahirajustice/customersupport/common/constants/SecurityConstants.java
+++ src/main/java/com/ahirajustice/customersupport/common/constants/SecurityConstants.java
@@ -6,11 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityConstants {
 
-    // Auth
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
-
-    // URLs
     public static final String[] EXCLUDE_FROM_AUTH_URLS = new String[] {
             "/**, OPTIONS",
             "/, GET",
